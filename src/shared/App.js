c48d9293862a51a5595fb0879b3ce92615bee598
Band Ly <bandly93@gmail.com>
2018-06-14 13:25:41 -0700

    add google analytics code

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,6 +1,21 @@
-import React,{Component} from 'react';
+import React,{Component,Fragment} from 'react';
+
+
+//import ga from 'react-ga';
+//import {GA_CODE} from '../../config.json';
 
 class App extends Component{
+	/*
+	componentDidMount(){
+		ga.initialize(GA_CODE,{debug:false});
+		ga.pageview(this.props.location.pathname);
+	}
+	componentWillUpdate(nextProps){
+		if(nextProps.location.pathname !== this.props.location.pathname){
+			ga.pageview(nextProps.location.pathname);
+		}
+	}
+	*/
 	render(){
 		return<div>
 			<h1>Welcome to Visuals by David Ho! </h1>
