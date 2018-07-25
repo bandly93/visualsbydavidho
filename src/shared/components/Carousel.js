917228e61f233232a23237a84f163a040e827166
iDatLe <LeDatDac@gmail.com>
2018-07-25 13:16:05 -0700

    Made carousel component reuseable

--- src/shared/components/Carousel.js
+++ src/shared/components/Carousel.js
@@ -3,18 +3,20 @@ import { connect } from 'react-redux';
 import { leftCarousel } from '../redux/actions/actions.js';
 import { rightCarousel } from '../redux/actions/actions.js';
 import styles from '../css/Carousel.css';
-const carouselData = require('../data/carouselData.js');
 
-const { imgUrl, imgText } = carouselData
+// Notes at bottom
 
 class Carousel extends Component {
+    constructor(props) {
+        super(props);
+    }
 
     previousSlide = () => {
         const currentImageIndex = this.props.imageCarouselData.currentImageIndex
         const newIndex = currentImageIndex - 1
         
         if (newIndex < 0) {
-            let newIndex = imgUrl.length - 1
+            let newIndex = this.props.pictures.length - 1
             this.props.nextSlide(newIndex)
         } else {
             this.props.nextSlide(newIndex)
@@ -25,7 +27,7 @@ class Carousel extends Component {
         const currentImageIndex = this.props.imageCarouselData.currentImageIndex
         const newIndex = currentImageIndex +  1
 
-        if (newIndex > imgUrl.length - 1) {
+        if (newIndex > this.props.backgroundImage.length - 1) {
             let newIndex = 0;
             this.props.nextSlide(newIndex)
         } else {
@@ -34,9 +36,8 @@ class Carousel extends Component {
     }
 
     render() {
-
         const backgroundStyle = {
-            backgroundImage: `url(${imgUrl[this.props.imageCarouselData.currentImageIndex]})`,
+            backgroundImage: `url(${this.props.backgroundImage[this.props.imageCarouselData.currentImageIndex]})`,
         }
         return(
             <div>
@@ -44,23 +45,22 @@ class Carousel extends Component {
                     className="carousel" 
                     style={backgroundStyle}>
                 </div>
+                
                 <div className="carouselText">
-                    {imgText[this.props.imageCarouselData.currentImageIndex]}
+                    {this.props.text[this.props.imageCarouselData.currentImageIndex]}
                 </div>
 
                     <div className="rightButtonBox">
-                        <div className="carouselButtonRight" onClick={this.nextSlide} />
+                        <div className="carouselButtonRight" onClick={this.nextSlide}>&#62;</div>
                     </div>
 
                     <div className="leftButtonBox">
-                        <div className="carouselButtonLeft" onClick={this.previousSlide} />
+                        <div className="carouselButtonLeft" onClick={this.previousSlide}>&#60;</div>
                     </div>
             </div>
         )
     }
 }
-
-
 
 
 function mapStateToProps(state) {
@@ -81,4 +81,12 @@ function mapDispatchToProps(dispatch) {
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Carousel)
-// export default Carousel;
+
+// This component is completely reusable. 
+// When passing data from higher order component, pass in as "backgroundImage" and "text"
+// The constructor is to have other higher order component pass background images in as backgrounImage
+// and the bottom left text as "text"
+// This will keep redux store clean from these cluttered data and have this component reusable
+
+// Note to self: When making this all fetched data, fetch data into array and put into redux store
+// then use that state to replace current redux state.
--- src/shared/pages/FrontPage.js
+++ src/shared/pages/FrontPage.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
 import Carousel from '../components/Carousel.js';
+const carouselData = require('../data/carouselData.js');
+
+const { imgUrl, imgText } = carouselData
 
 class FrontPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            imageUrl: imgUrl,
+            imageText: imgText
+        }
+    }
+
     render() {
+
         return(
             <div>
-                <Carousel />
+                <Carousel 
+                backgroundImage={this.state.imageUrl} 
+                text={this.state.imageText} />
             </div>
         )
     }
